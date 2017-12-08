a48c0ab88038462b7d2eaa76c8ca3a5459f854bb
George Correa <georgecorrea@Georges-MacBook-Pro.local>
2017-12-08 18:23:14 -0500

    Add Game Center and Leaderboard.

--- BreakoutChallenge/GameViewController.swift
+++ BreakoutChallenge/GameViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+import GameKit
 
 class GameViewController: UIViewController {
     
@@ -8,8 +9,18 @@ class GameViewController: UIViewController {
     @IBOutlet weak var currentScoreLabel: UILabel!
     @IBOutlet weak var finalScoreLabel: UILabel!
     
+    var gcEnabled = Bool() // Check if the user has Game Center enabled
+    var gcDefaultLeaderBoard = String() // Check the default leaderboardID
+    
+//    var score = 0
+    
+    // IMPORTANT: replace the red string below with your own Leaderboard ID (the one you've set in iTunes Connect)
+    let LEADERBOARD_ID = "com.score.BreakoutChallenge"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        authenticateLocalPlayer()
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
@@ -25,7 +36,43 @@ class GameViewController: UIViewController {
             view.ignoresSiblingOrder = true
         }
     }
-
+    
+    // MARK: - AUTHENTICATE LOCAL PLAYER
+    func authenticateLocalPlayer() {
+        let localPlayer: GKLocalPlayer = GKLocalPlayer.localPlayer()
+        
+        localPlayer.authenticateHandler = {(ViewController, error) -> Void in
+            if((ViewController) != nil) {
+                // 1. Show login if player is not logged in
+                self.present(ViewController!, animated: true, completion: nil)
+            } else if (localPlayer.isAuthenticated) {
+                // 2. Player is already authenticated & logged in, load game center
+                self.gcEnabled = true
+                
+                // Get the default leaderboard ID
+                localPlayer.loadDefaultLeaderboardIdentifier(completionHandler: { (leaderboardIdentifer, error) in
+                    if error != nil { print(error as Any)
+                    } else { self.gcDefaultLeaderBoard = leaderboardIdentifer! }
+                })
+                
+            } else {
+                // 3. Game center is not enabled on the users device
+                self.gcEnabled = false
+                print("Local player could not be authenticated!")
+                print(error as Any)
+            }
+        }
+    }
+    
+    // Action that allows you to check Top Scores Leaderboard in Game Center
+    @IBAction func checkLeaderboard(_ sender: UIButton) {
+        let gcVC = GKGameCenterViewController()
+        gcVC.gameCenterDelegate = self
+        gcVC.viewState = .leaderboards
+        gcVC.leaderboardIdentifier = LEADERBOARD_ID
+        present(gcVC, animated: true, completion: nil)
+    }
+    
     override var shouldAutorotate: Bool {
         return true
     }
@@ -48,6 +95,12 @@ class GameViewController: UIViewController {
     }
 }
 
+extension GameViewController: GKGameCenterControllerDelegate {
+    func gameCenterViewControllerDidFinish(_ gameCenterViewController: GKGameCenterViewController) {
+        gameCenterViewController.dismiss(animated: true, completion: nil)
+    }
+}
+
 extension GameViewController: RefreshLabelsDelegate {
     func beginGame() {
         stageScoreLabel.text = "Stage Score: \(GameScene.stageScore)"
@@ -64,5 +117,16 @@ extension GameViewController: RefreshLabelsDelegate {
         finalScoreLabel.text = "Final Score: \(GameScene.currentScore)"
         view.bringSubview(toFront: finalScoreLabel)
         finalScoreLabel.isHidden = false
+        
+        // Submit score to Game Center
+        let bestScoreInt = GKScore(leaderboardIdentifier: LEADERBOARD_ID)
+        bestScoreInt.value = Int64(GameScene.currentScore)
+        GKScore.report([bestScoreInt]) { (error) in
+            if error != nil {
+                print(error!.localizedDescription)
+            } else {
+                print("Best Score submitted to your Leaderboard!")
+            }
+        }
     }
 }
